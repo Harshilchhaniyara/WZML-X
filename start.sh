7dbfe4c713e9b85725164b0553e0fa2ccf4f45e7
justatest420 <justatestsubject.01@gmail.com>
2025-01-10 12:13:30 +0545

    Update start.sh
    Signed-off-by: justatest420 <justatestsubject.01@gmail.com>

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
-gunicorn app:app && python3 update.py && python3 -m bot
+#!/bin/bash
+
+# Start Gunicorn in the background
+gunicorn app:app &
+
+# Run the other scripts
+python3 update.py
+python3 -m bot
+
+# Wait for all background jobs to finish
+wait
